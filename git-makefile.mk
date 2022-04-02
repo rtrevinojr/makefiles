e898ae5568b87892f88a1e39ec159bcf95a555a8
H. Rene Trevino Jr <rtrevinojr@utexas.edu>
2022-04-01 19:07:53 -0500

    Update git-makefile.mk
    Git stash

--- git-makefile.mk
+++ git-makefile.mk
@@ -49,7 +49,76 @@ git/delete/branch/r:
     git checkout --patch ${source-branch} ${file-name}
     
  # checkout previous commit into a new branch
+ git/checkout/revert:
     git checkout ${hash-id} -b ${branch-name}
     # alternative (make sure to use period .)
     git checkout ${hash-id} .
+    
+ # stash
+ 
+# Git notes #
+
+## Git delete branch ##
+
+- Delete branch
+~~~
+$ git branch -d <branch-name>
+~~~
+
+- Delete branch (FORCE)
+~~~
+$ git branch -D <branch-name>
+~~~
+
+- Delete Remote branch
+~~~
+$ git branch origin --delete <branch-name>
+~~~
+
+## Git stash ##
+
+# - Stash Staged & Unstaged changes
+git/stash:
+  $ git stash
+
+
+# - Pop (most recently stashed changes) from working directory, and reapplies those changes to working directory
+git/stash/pop:
+  git stash pop
+
+
+# - Alternatively, git stash apply will remove the changes from your working directory, but unlike pop, apply wil keep the stashed changes
+git/stash/apply:
+  git stash apply <stash-id>
+
+
+# - Stash untracked changes
+git/stash/n:
+  git stash -n 
+
+
+# - List Stash commit id's 
+git/stash/list:
+  git stash list
+
+
+# - Delete stash commit
+git/stash/delete:
+  git delete <stash-id>
+
+
+# - Merge specific file (only) on separate branch
+# 1. get file path
+git/path:
+  git ls-files --full-name <file-name>
+
+# 2. checkout file
+git/checkout/file:
+  git checkout <branch-name> <file-name>
+
+# 3. merge file
+# $ git merge 
+
+
+
   
