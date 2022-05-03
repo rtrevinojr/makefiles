2b1c76a76385659b2215966acb2823dcd8e3cc41
H. Rene Trevino Jr <rtrevinojr@utexas.edu>
2022-05-03 17:20:13 -0500

    Update k8s-makefile.mk

--- k8s-makefile.mk
+++ k8s-makefile.mk
@@ -3,7 +3,23 @@ hello:
  		echo 'k8s makefile'
 		
 
-nodes:
+nodes/describe:
 	kubectl -n ${namespace} desribe nodes 
+	
+nodes:
+	kubectl -n ${namespace} get nodes
+	
+nodes/view-labels:
+	kubectl -n ${namespace} get nodes --view-labels
+	
+# create label on node
+label/node:
+	kubectl label node ${node-name} ${key}=${value}
+	
+# delete label on node
+label/node/delete:
+	kubectl label node ${node-name} -${key}
+	
+
 	
   
