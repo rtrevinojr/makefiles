dbb4949fafe5681ce64d37a1c868329084fc5513
H. Rene Trevino Jr <rtrevinojr@utexas.edu>
2023-01-03 17:57:13 -0600

    Update linux-cli.mk
    added \Alt commands

--- linux-cli.mk
+++ linux-cli.mk
@@ -1,20 +1,68 @@
 
-## linux cli (command line interface) shortcuts ##
+### linux cli (command line interface) shortcuts ###
 
-# 1 - \Ctrl + a     --> HOME
-# 2 - \Ctrl + e     --> END
-# 3 - \Ctrl + h     --> [backspace] <--
-# 4 - \Ctrl + l     --> clear
-# 5 - \Ctrl + r     --> search history 
-# 6 - \Ctrl + k     --> delete til END
-# 7 - \Ctrl + w     --> delete word
-# 8 - \Ctrl + u     --> delete til HOME
-# 9 - \Ctrl + f     --> move (forward) one char
-# 10 - \Ctrl + d     --> delete char to the right
+#  - \Ctrl + v     --> version
 
-# 10 - \Ctrl + d     --> delete char to the right
-# 11 - \Ctrl + t     --> replace char left of cursor with right char
-# 12 - \Ctrl + y     --> paste clipboard
-# 13 - \Ctrl + b     --> move (backwards) one char
-# 14 - \Ctrl + v     --> version
-# 14 - \Ctrl + p     --> ?
+## Process control ##
+
+# - \Ctrl + c     --> interrupt (kill) the current foreground process running in the terminal (SIGINT ignal sent to process)
+# - \Ctrl + z     --> suspend the current foregrouind process running in bash (SIGTSTP signal sent to process) use fg <pid> to return process to foreground later
+# - \Ctrl + d     --> close bash shell (sends EOF end-of-file marker to bash, & bash exits when it receives this marker) similar to running exit command
+
+## screen control ##
+
+# - \Ctrl + l     --> clear
+# - \Ctrl + s     --> stop all output to the screen (useful for disregarding logs to console output without \Ctrl+c
+# - \Ctrl + q     --> resume output to the screen after stopping it with \Ctrl+s
+
+
+## moving cursor ##
+
+# - \Ctrl + a     --> HOME
+# - \Ctrl + e     --> END
+
+# - \Ctrl + b     --> go left (backwards) one character
+# - \Alt + e     --> go left (backwards) one word
+
+# - \Ctrl + f     --> go right (forwards) one character
+# - \Alt + f      --> go right (forwards) one word
+
+# - \Ctrl + b     --> move between the beginning of the line and the current position of the cursor
+                  
+
+## deleting ##
+
+#  - \Ctrl + d     --> delete character under cursor [delete]
+#  - \Alt + d     --> delete all characters under cursor on current line
+# - \Ctrl + h     --> [backspace] <--
+
+## undo ##
+
+#  - \Alt + _      --> undo last key press 
+#  - \Alt + t      --> swap current word with the previous word
+#  - \Ctrl + t      --> swap last two characters before cursor 
+
+## Cut/Paste ##
+
+# - \Ctrl + w     --> cut (delete) word (word left of cursor)
+# - \Ctrl + k     --> cut til END
+# - \Ctrl + u     --> cut til HOME
+# - \Ctrl + y     --> paste clipboard (yank)
+
+
+## history ##
+
+#  - \Ctrl + r     --> search history 
+#  - \Ctrl + p     --> previous command within history
+#  - \Ctrl + n     --> next command within history
+#  - \Alt + r      --> revert any changes to command that was pulled from history after editing
+#  - \Ctrl + o     --> run command retrieved with \Ctrl+r
+#  - \Ctrl + g     --> exit history searching mode with \Ctrl+r
+
+## Capitalization ##
+
+#  - \Alt + u      --> capitalize every character from cursor til end of current word
+#  - \Alt + u      --> uncapitalize every character from cursor til end of current word
+#  - \Alt + c      --> capitalize character under cursor and move cursor to end of word
+
+
