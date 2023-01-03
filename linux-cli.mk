9cc5a7abb79b2105fdc15cf3e2a507e5a9e31d07
H. Rene Trevino Jr <rtrevinojr@utexas.edu>
2023-01-03 17:30:20 -0600

    Create linux-cli.mk
    linux cli shortcuts

---
+++ linux-cli.mk
@@ -0,0 +1,19 @@
+
+## linux cli (command line interface) shortcuts ##
+
+# 1 - \Ctrl + a     --> HOME
+# 2 - \Ctrl + e     --> END
+# 3 - \Ctrl + h     --> [backspace] <--
+# 4 - \Ctrl + l     --> clear
+# 5 - \Ctrl + r     --> search history 
+# 6 - \Ctrl + k     --> delete til END
+# 7 - \Ctrl + w     --> delete word
+# 8 - \Ctrl + u     --> delete til HOME
+# 9 - \Ctrl + f     --> move (forward) one char
+# 10 - \Ctrl + d     --> delete char to the right
+
+# 10 - \Ctrl + d     --> delete char to the right
+# 11 - \Ctrl + t     --> replace char left of cursor with right char
+# 12 - \Ctrl + y     --> paste clipboard
+# 13 - \Ctrl + b     --> move (backwards) one char
+# 14 - \Ctrl + p     --> ?
